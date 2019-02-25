6ddda9c2642e3b6eace452894805849fab79637e
derweissraum <derweissraum@users.noreply.github.com>
2019-02-25 23:46:23 +0100

    Update smooth-scroll.js
    Smooth Scroll Bug

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 	// Smooth scrolling when clicking on a hash link
 
       $(function () {
-        $('a[href^="#"]').click(function () { // Klick auf den Button
+        $('a[href^="#top"]').click(function () { // Klick auf den Button
           $('body,html').animate({
             scrollTop: 0
           }, 800);
